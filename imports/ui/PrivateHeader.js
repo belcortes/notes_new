ecb77d44e9249fc7b7aa5fa91d18f8845134b0a7
belcortes <belcortes@gmail.com>
2018-02-26 19:05:29 -0800

    adding styling

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -8,7 +8,7 @@ export const PrivateHeader = (props) => {
     <div className="header">
       <div className="header__content">
         <h1 className="header__title">{props.title}</h1>
-        <button className="button button--link-text" onClick={() => props.handleLogout()}>Logout</button>
+        <button className="button button--link-text" onClick={() => props.handleLogout()}>LOGOUT</button>
       </div>
     </div>
   );
